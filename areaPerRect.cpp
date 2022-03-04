5113b7e8f563a5af0feaf36e309b9c90feee56be
Ubuntu <ubuntu@ip-172-31-85-213.ec2.internal>
2022-03-04 01:39:14 +0000

    Basic math and area & Perimeter of a Rectangle

---
+++ areaPerRect.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) Year Your Name All rights reserved.
+
+//
+
+// Created by: Ferdaws
+
+// Date: 3/3/2022
+
+// This program shows the dimensions of a rectangle
+// and calculates the area and perimeter of it.
+
+
+#include <iostream>
+
+int main() {
+  std::cout << "If a rectangle has the dimensions: " << std::endl;
+  std::cout << "4cm x 2cm" << std::endl;
+  std::cout << "" << std::endl;
+  std::cout << "Area is:" << (4*2) << "cm^2" << std::endl;
+  std::cout << "Perimeter is:" << (2*(4+2)) << "cm" << std::endl;
+}
---
+++ basicMath.cpp
@@ -0,0 +1,22 @@
+// Copyright (c) Year Your Name All rights reserved.
+
+//
+
+// Created by: Ferdaws
+
+// Date: 3/3/2022
+
+// This program displays simple math problems and the answer to them. The important functions are division where the answer is a decimal, powers and square roots.
+
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+     std::cout << "8+2=" << (8+2) << std::endl;
+     std::cout << "7-1=" << (7-1) << std::endl;
+     std::cout << "2^4=" << (pow(2 , 4)) << std::endl;
+     std::cout << "8/3=" << (float(8)/float(3)) << std::endl;
+     std::cout << "√16=" << pow(16 , 0.5) << std::endl;
+     std::cout << "8*2=" << (8*2) << std::endl;
+}
